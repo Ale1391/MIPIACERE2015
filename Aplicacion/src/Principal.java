bd2974eaab00b0755a873e7cae3ebfdeae40eed9
Ale1391 <alejandro.m.napoli@gmail.com>
2014-12-31 18:35:39 -0300

    Proyecto en IntelliJ

---
+++ Aplicacion/src/Principal.java
@@ -0,0 +1,16 @@
+import aplicacion.funcionalidades.Ingreso;
+
+/**
+ * Created by Papa Frita on 31/12/2014.
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Ingreso fIngreso = new Ingreso();
+        fIngreso.setVisible(true);
+
+
+    }
+
+}
---
+++ Aplicacion/src/aplicacion/funcionalidades/Ingreso.java
@@ -0,0 +1,19 @@
+package aplicacion.funcionalidades;
+
+import javax.swing.*;
+
+/**
+ * Created by Papa Frita on 31/12/2014.
+ */
+public class Ingreso extends JFrame {
+
+    // Constructor
+    public Ingreso() {
+        setTitle("Ingreso al sistema");
+        setResizable(false);
+        setSize(400, 200);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
