fa1118a502a6821d885da3e5e96e0c683a624aec
virgen-en-parapente <seba_cai18@hotmail.com>
2015-01-07 00:45:54 -0300

    gui un toque mas prolijo

--- Aplicacion/src/aplicacion/funcionalidades/Ingreso.java
+++ Aplicacion/src/aplicacion/funcionalidades/Ingreso.java
@@ -1,50 +1,67 @@
 package aplicacion.funcionalidades;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by Papa Frita on 31/12/2014.
  */
-public class Ingreso extends JFrame {
+public class Ingreso extends JFrame implements ActionListener {
+
+    JFrame ventana;
+    JPanel panel;
+    JButton boton1;
+    JButton boton2;
+    JButton boton3;
+    JButton boton4;
+    JButton boton5;
+    JLabel etiqueta1;
+    JLabel etiqueta2;
 
     // Constructor
     public Ingreso() {
- //    setTitle("Ingreso al sistema");
-  //    setResizable(false);
-  //    setLayout(null);
-        JFrame ventana = new JFrame();
-        JPanel panel = new JPanel();
-        JButton boton1 = new JButton();
-        JButton boton2 = new JButton();
-        JButton boton3 = new JButton();
-        JButton boton4 = new JButton();
-        JButton boton5 = new JButton();
+        gui();
+    }
 
-        JLabel etiqueta = new JLabel();
-        JLabel etiqueta2 = new JLabel();
+    private void gui(){
 
-        etiqueta.setText("Contabilidad");
+        ventana = new JFrame();
+        panel   = new JPanel();
+        boton1  = new JButton("Stock");
+        boton2  = new JButton("Datos");
+        boton3  = new JButton("Informes Generales");
+        boton4  = new JButton("Parametros Generales");
+        boton5  = new JButton("Herramientas Avanzadas");
+
+        etiqueta1 = new JLabel();
+        etiqueta2 = new JLabel();
+
+        etiqueta1.setText("Contabilidad");
         etiqueta2.setText("Operaciones avanzadas");
 
-        boton1.setText("Stock");
-        boton2.setText("Datos");
-        boton3.setText("Informes Generales");
-        boton4.setText("Parametros Generales");
-        boton5.setText("Herramientas Avanzadas");
-
-        panel.add(etiqueta);
-        panel.add (boton1);
+        panel.add(etiqueta1);
+        panel.add(boton1);
         panel.add(boton2);
         panel.add(boton3);
         panel.add(etiqueta2);
         panel.add(boton4);
         panel.add(boton5);
 
+        boton1.addActionListener(this);
+
         ventana.add (panel);
         ventana.setSize(800, 600);
         ventana.setTitle("Pantalla Principal");
         ventana.setVisible(true);
-     //   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //    setTitle("Ingreso al sistema");
+        //    setResizable(false);
+        //    setLayout(null);
     }
 
+    public void actionPerformed (ActionEvent evt){
+        JOptionPane.showMessageDialog(this,"entraste a stock");
+    }
 }
