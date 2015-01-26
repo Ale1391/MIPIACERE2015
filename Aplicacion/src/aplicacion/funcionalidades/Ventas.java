eff1d00a2b7563ccd66bba433ba944d6df5f5fd3
Ale1391 <alejandro.m.napoli@gmail.com>
2015-01-25 23:54:40 -0300

    Mejoras en módulo de Ventas
    Cuestiones gráficas básicas.

--- Aplicacion/src/aplicacion/funcionalidades/Ventas.java
+++ Aplicacion/src/aplicacion/funcionalidades/Ventas.java
@@ -7,48 +7,53 @@ import javax.swing.*;
  */
 public class Ventas extends JFrame {
 
-    // Se definen los elementos graficos que apareceran en la ventana
-    JLabel texto1;
-    JButton botonOperacion;
-    JButton botonComprobante;
-    JButton botonConfig;
+    // Se declaran los elementos graficos que apareceran en la ventana
+    JLabel txtInfo;
+    JButton btnOperacion;
+    JButton btnComprobante;
+    JButton btnConfig;
 
-    // Constructor
-    public Ventas () {
+    // CONSTRUCTOR
+    public Ventas() {
 
+    // 1) CUESTIONES GENERALES
         // Se define el titulo de la ventana
         setTitle("Ventas");
         // No permite estirar ventana
         setResizable(false);
-        setSize(900,900);
+        setSize(700, 700);
         // Sin uso de plantilla de distribucion de elementos
         setLayout(null);
         // Cuando cierra se saca el proceso de ejecucion
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Definicion de objetos
-        texto1 = new JLabel("Ingrese la opcion deseada:");
-        botonConfig = new JButton("Configuracion de compras");
-        botonOperacion = new JButton("Registrar operacion");
-        botonComprobante = new JButton("Registrar comprobante");
-
+    // 2) DEFINICION DE OBJETOS
+        txtInfo = new JLabel("Ingrese la opcion deseada:");
+        btnOperacion = new JButton("Registrar operacion");
+        btnComprobante = new JButton("Registrar comprobante");
+        btnConfig = new JButton("Configuracion de compras");
+/*
         // Definicion de propiedades de los objetos ???????
         botonConfig.setHorizontalTextPosition(SwingConstants.HORIZONTAL);
         botonConfig.setVerticalTextPosition(SwingConstants.BOTTOM);
         botonConfig.setToolTipText("Ingresa a panel de configuracion");
+*/
+    // 3) AGREGADO DE OBJETOS
+        add(txtInfo);
+        add(btnOperacion);
+        add(btnComprobante);
+        add(btnConfig);
 
-        // Se agregan los elementos a la ventana
-        add(texto1);
-        add(botonConfig);
-        add(botonOperacion);
-        add(botonComprobante);
+    // 4) UBICACION DE OBJETOS
+        txtInfo.setBounds(200, 50, 200, 25);
+        btnOperacion.setBounds(200, 100, 150, 25);
+        btnComprobante.setBounds(200, 150, 150, 25);
+        btnConfig.setBounds(200, 200, 150, 25);
 
-        // Se ubican los elementos en la ventana
-        texto1.setBounds(500, 200, 200, 250);
-        botonConfig.reshape(20, 30, 200, 230);
-        botonOperacion.reshape(20, 30, 200, 230);
-        botonComprobante.reshape(20, 30, 200, 230);
 
+    // OTROS
+        // Centra la ventana
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
 
     }
