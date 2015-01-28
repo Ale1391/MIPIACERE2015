323845a9a09ba44224897c62fe5b39a3605d9cb1
virgen-en-parapente <seba_cai18@hotmail.com>
2015-01-27 23:27:55 -0300

    Modulo Empresa (ventana)

--- Aplicacion/src/Principal.java
+++ Aplicacion/src/Principal.java
@@ -1,3 +1,4 @@
+import aplicacion.Empresa;
 import aplicacion.Ingreso;
 import aplicacion.funcionalidades.Ventas;
 
@@ -8,8 +9,8 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        Ingreso fIngreso = new Ingreso();
-       // fIngreso.setVisible(true);
+        Empresa fempresa = new Empresa();
+
 
         // Esto es temporal, para prueba de formularios con acceso no creado
         Ventas fVentas = new Ventas();
--- Aplicacion/src/aplicacion/Ingreso.java
+++ Aplicacion/src/aplicacion/Ingreso.java
@@ -5,63 +5,8 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-/**
- * Created by Papa Frita on 31/12/2014.
- */
-public class Ingreso extends JFrame implements ActionListener {
+/** comentado los referente al click*/
 
-    JFrame ventana;
-    JPanel panel;
-    JButton boton1;
-    JButton boton2;
-    JButton boton3;
-    JButton boton4;
-    JButton boton5;
-    JLabel etiqueta1;
-    JLabel etiqueta2;
+public class Ingreso {
 
-    // Constructor
-    public Ingreso() {
-        gui();
-    }
-
-    private void gui(){
-
-        ventana = new JFrame();
-        panel   = new JPanel();
-        boton1  = new JButton("Stock");
-        boton2  = new JButton("Datos");
-        boton3  = new JButton("Informes Generales");
-        boton4  = new JButton("Parametros Generales");
-        boton5  = new JButton("Herramientas Avanzadas");
-
-        etiqueta1 = new JLabel();
-        etiqueta2 = new JLabel();
-
-        etiqueta1.setText("Contabilidad");
-        etiqueta2.setText("Operaciones avanzadas");
-
-        panel.add(etiqueta1);
-        panel.add(boton1);
-        panel.add(boton2);
-        panel.add(boton3);
-        panel.add(etiqueta2);
-        panel.add(boton4);
-        panel.add(boton5);
-
-        boton1.addActionListener(this);
-
-        ventana.add (panel);
-        ventana.setSize(800, 600);
-        ventana.setTitle("Pantalla Principal");
-        ventana.setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //    setResizable(false);
-        //    setLayout(null);
-    }
-
-    public void actionPerformed (ActionEvent evt){
-        JOptionPane.showMessageDialog(this,"entraste a stock");
-    }
 }
