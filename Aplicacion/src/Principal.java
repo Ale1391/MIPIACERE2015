ff020febb01e6b494a03b4b70135b17c99efe053
Ale1391 <alejandro.m.napoli@gmail.com>
2015-01-31 21:38:02 -0300

    Carpeta para bibliotecas y prueba de conexion a Base de datos
    - Para el archivo .jar hacer click derecho y opción "As a library"
    - Crear una base en C:\SQLite con el nombre "prueba.db"
    - Crear un objeto Data Source apuntando al driver y a la base

--- Aplicacion/src/Principal.java
+++ Aplicacion/src/Principal.java
@@ -1,6 +1,7 @@
 import aplicacion.Empresa;
 import aplicacion.Ingreso;
 import aplicacion.funcionalidades.Ventas;
+import java.sql.*;
 
 /**
  * Created by Papa Frita on 31/12/2014.
@@ -15,7 +16,17 @@ public class Principal {
         // Esto es temporal, para prueba de formularios con acceso no creado
         Ventas fVentas = new Ventas();
 
+        //////////// Prueba de base de datos /////////////
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");            //????????
+            c = DriverManager.getConnection("jdbc:sqlite:C:\\SQLite\\prueba.db");       //Driver y ruta de la base
+            System.out.println("Conexion a la base de datos realizada correctamente.");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.out.println("Conexion a la base de datos NO pudo ser realizada.");
+            System.exit(0);
+        }
 
     }
-
 }
